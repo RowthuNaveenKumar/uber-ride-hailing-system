ecfaaea5f7b37fa6784d3a715612efa6a9030c9d
Rowthu Naveen Kumar <naveenkumar910016@gmail.com>
2026-01-04 17:30:12 +0530

    Secure config and stable backend setup

--- uber_system/src/main/java/com/ridehailing/uber_system/config/SecurityConfig.java
+++ uber_system/src/main/java/com/ridehailing/uber_system/config/SecurityConfig.java
@@ -60,37 +60,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-
-
-
-
-    //    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .cors(Customizer.withDefaults())
-//                .csrf(csrf->csrf.disable())
-//                .sessionManagement(session->
-//                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                )
-//                .authorizeHttpRequests(auth->auth
-//                        .requestMatchers(
-//                                "/auth/**",
-//                                "/test/**",
-//                                "/driver/signup",
-//                                "/driver/login").permitAll()
-//                        .requestMatchers(
-//                                        "/ws/**",
-//                                        "/topic/**",
-//                                        "/app/**").permitAll()
-//                        .requestMatchers("/driver/**").hasRole("DRIVER")
-//                        .requestMatchers("/ride/view/**").hasRole("CUSTOMER")//---
-//                        .requestMatchers("/ride/driver/**").hasRole("DRIVER")
-//                        .requestMatchers("/ride/**").hasRole("CUSTOMER")
-//                        .anyRequest().authenticated()
-//                )
-//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-//        return http.build();
-//    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
